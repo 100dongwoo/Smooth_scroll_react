1983fe0b2dbc8ddb0014f7c3da114117e2e2c7c0
BAEK DONGWOO <qorehddn123@naver.com>
2020-12-24 15:13:25 +0900

    Clear navbar

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # React & SmoothScroll
+
 ### 처음알게된 정보
+- styled-component Active 사용법
+![CommentStructure](./studyPic/styledComponentActive.png) <br/>
 - 
-![CommentStructure](./studyPic/) <br/>
 
 ## 찾아볼것 
 - sans-serif
+- justify-self
--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
 function App() {
     return (
-        <div>
+        <Router>
             <Navbar />
-        </div>
+        </Router>
     );
 }
 
