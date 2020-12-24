1ea4a6a8aa864627be786c98a53304a97a378b37
BAEK DONGWOO <qorehddn123@naver.com>
2020-12-24 16:09:26 +0900

    sidebar Clear

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import {
+    SidebarContainer,
+    Icon,
+    CloseIcon,
+    SidebarLink,
+    SidebarRoute,
+    SidebarWrapper,
+    SideBtnWrap,
+    SidebarMenu,
+} from './SidebarElement';
 
 function Sidebar(props) {
-    return <div></div>;
+    return (
+        <SidebarContainer>
+            <Icon>
+                <CloseIcon />
+            </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="about">About</SidebarLink>
+                    <SidebarLink to="discover">Discover</SidebarLink>
+                    <SidebarLink to="services">Services</SidebarLink>
+                    <SidebarLink to="signup">Sign UP</SidebarLink>
+                </SidebarMenu>
+                <SideBtnWrap>
+                    <SidebarRoute to="/signin">Sign In</SidebarRoute>
+                </SideBtnWrap>
+            </SidebarWrapper>
+        </SidebarContainer>
+    );
 }
 
 export default Sidebar;
