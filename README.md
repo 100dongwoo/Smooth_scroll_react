d1401ce86e5c3dbfe1ba8a9f7264097c31b682d8
BAEK DONGWOO <qorehddn123@naver.com>
2020-12-24 14:31:35 +0900

    구조 잡기

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # React & SmoothScroll
 ### 처음알게된 정보
+- 
+![CommentStructure](./studyPic/) <br/>
 
 ## 찾아볼것 
+- sans-serif
---
+++ src/components/Navbar/NavarElements.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NavarElements(props) {
+    return <div></div>;
+}
+
+export default NavarElements;
